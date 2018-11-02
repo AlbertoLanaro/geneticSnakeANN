- introduced confidential interval for top mean fitness calculation - Snake mutation probability is fixed and DNA mutation probability decreases with the mean fitness of the top snakes

src/simulation.py
@@ -6,6 +6,7 @@ import random
 import conf
 import numpy as np
 import time
+import scipy.stats as st
 
 N = conf.N_CROSS
 
@@ -64,7 +65,7 @@ class Simulation:
     Function that upgrate generation with a different distribution of parents
     It choose the parents with higher fitness with more prbability
     We   1)choose the N parents best parents of the simulation
-         2) Create the vector of their ftinesses
+         2) Create the vector of their fitnesses
          3) CumSum the fitnesses vector
          4) Create a random int from 0 to sum(fitnesses)
          5) Choose the snake which fitness is nearer ahead the random int
@@ -73,53 +74,58 @@ class Simulation:
         # sort for fitness
         self.sortSnakesForFitness()        
         max_fit = self.geneticSnakes[-1].fitness
+        min_fit = self.geneticSnakes[0].fitness
+        # store snake's DNA
         if max_fit > conf.MAX_FITNESS:
             self.geneticSnakes[-1].brain.DNAsave(max_fit)
             conf.MAX_FITNESS = max_fit
-        min_fit = self.geneticSnakes[0].fitness
-        # Creating the fitnesses vector
-        fit_array = []
+        # Creating the fitnesses cumulative density function
+        fit_array_cdf = []
         for i in self.geneticSnakes[self.n_snakes - conf.N_CROSS:]:
-            fit_array.append(i.fitness**2)
-        fit_array = np.cumsum(np.array(fit_array))
-        # Taking the (n_snakes - N_SNAKE_SURVIVING) worst snakes and 
-        # substitute them with other reproduced from the better snakes
-        fit = 0
-        fit_top = 0
-        average_distribution = 0
+            fit_array_cdf.append(i.fitness**2)
+        fit_array_cdf = np.cumsum(np.array(fit_array_cdf))
+        fit = []
+        fit_top = []
+        parents_fitness = 0
         for i in self.geneticSnakes[: self.n_snakes - conf.N_CROSS]:
-            fit += i.fitness
+            fit.append(i.fitness)
+        # top-performing snakes used for reproduction
         for i in self.geneticSnakes[self.n_snakes - conf.N_CROSS:]:
-            fit_top += i.fitness
-        mean = (fit + fit_top)/self.n_snakes
-        variance = 0 
-        for i in self.geneticSnakes:
-            variance += (i.fitness - mean)**2
-        variance = variance/self.n_snakes
-        # 1. new snakes (with mutation)
+            fit_top.append(i.fitness)
+        
+        # generation statistics
+        mean_fit = np.mean(fit)
+        # confidence interval (95%)
+        mean_top_fit = np.mean(fit_top)
+        top_fit_CI = st.t.interval(0.95, len(fit_top)-1, loc=mean_top_fit, scale=st.sem(fit_top))
+
+        # DNA mutation probability
+        p_mutation = min(conf.MUTATION_RATE, conf.MUTATION_RATE / (mean_top_fit))
+        
+        # Taking the (n_snakes - N_SNAKE_SURVIVING) worst snakes and 
+        # substitute them with other reproduced from the best snakes
+        # 1. new snakes (with probable mutation)
         for i in self.geneticSnakes[:self.n_snakes - conf.N_SNAKE_SURVIVING]:
             rnd = random.random()
-            if rnd > (conf.MUTATION_PROBABILITY / fit_top ** 2):
-                random_index0 = random_array(fit_array)
-                random_index1 = random_array(fit_array)
-                average_distribution += self.geneticSnakes[random_index1].fitness
-                i.brain.crossDNA(
-                    self.geneticSnakes[random_index0].brain, self.geneticSnakes[random_index1].brain)
-            else:
-                random_index0 = random_array(fit_array)
-                random_index1 = random_array(fit_array)
+            # do not mutate DNA
+            if rnd > conf.MUTATION_PROBABILITY:
+                random_index0 = random_sampling(fit_array_cdf)
+                random_index1 = random_sampling(fit_array_cdf)
+                parents_fitness += self.geneticSnakes[random_index1].fitness
+                i.brain.crossDNA(self.geneticSnakes[random_index0].brain, self.geneticSnakes[random_index1].brain)
+            # possibly mutate DNA
+            else: 
+                random_index0 = random_sampling(fit_array_cdf)
+                random_index1 = random_sampling(fit_array_cdf)
                 value = self.geneticSnakes[random_index1].fitness
-                average_distribution += value
-                i.brain.crossDNAAndMutate(
-                    self.geneticSnakes[random_index0].brain, self.geneticSnakes[random_index1].brain)
+                parents_fitness += value
+                i.brain.crossDNAAndMutate(self.geneticSnakes[random_index0].brain, self.geneticSnakes[random_index1].brain, p_mutation)
             i.clear()
         # 2. snakes that survive to the next generation
-        # 2.1 snakes that are not used for crossover
         for i in self.geneticSnakes[self.n_snakes - conf.N_SNAKE_SURVIVING:]:
             i.clear()
-        # 2.2 snakes with highest fitness that are used for crossover 
 
-        return mean, variance,  (fit_top/N), (average_distribution/(self.n_snakes- conf.N_SNAKE_SURVIVING)), max_fit, min_fit, self.iteration
+        return mean_fit, mean_top_fit, top_fit_CI, p_mutation, parents_fitness/(self.n_snakes- conf.N_SNAKE_SURVIVING), max_fit, min_fit, self.iteration
         
     '''
     Shows the snake with higher fitness
@@ -158,7 +164,7 @@ Utility function
 given a sorted array it gives a random index. 
 The distrubution is not uniform: it's proportional to the distance from the previus element
 '''
-def random_array(fit_array):
+def random_sampling(fit_array):
     rnd = random.randint(1, fit_array[-1])
     
     i = 1